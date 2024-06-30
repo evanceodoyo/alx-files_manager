2d15baae1b869a4e229f2eb32a87b84789f6ac98
Evance Odoyo <odoyoevanc@gmail.com>
2024-07-01 00:38:21 +0300

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const dbName = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${dbName}`;
+
+    this.client = new MongoClient(uri, {
+      useUnifiedTopology: true,
+    });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client && this.client.topology.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
