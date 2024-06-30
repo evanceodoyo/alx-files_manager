e6e180132bcc9049efb7628f2fe182798fc91ba6
Evance Odoyo <odoyoevanc@gmail.com>
2024-06-30 21:57:07 +0300

    conform to ESLint

--- utils/redis.js
+++ utils/redis.js
@@ -2,25 +2,25 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = createClient();
     this.client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.connected;
   }
 
-  async get (key) {
+  async get(key) {
     const value = await promisify(this.client.get).bind(this.client)(key);
     return value;
   }
 
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     await promisify(this.client.setex).bind(this.client)(key, duration, value);
   }
 
-  async del (key) {
+  async del(key) {
     await promisify(this.client.del).bind(this.client)(key);
   }
 }
